8abd2746266b83a5ed494e72ae3d143643fbee8f
svetlana <svet-ik@ukr.net>
2021-02-03 15:44:50 +0200

    Add search component and service

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,8 +1,9 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Course} from '../model/course';
-import {Observable, pipe} from 'rxjs';
+import {Observable} from 'rxjs';
 import {map, shareReplay} from 'rxjs/operators';
+import {Lesson} from '../model/lesson';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,26 @@ import {map, shareReplay} from 'rxjs/operators';
 export class CoursesService {
 
   constructor(private http: HttpClient) {
+  }
+
+  loadCourseById(courseId: number) {
+    return this.http.get<Course>(`/api/courses/${courseId}`)
+      .pipe(
+        shareReplay()
+      );
+  }
+
+  loadAllCourseLessons(courseId: number): Observable<Lesson[]> {
+    return this.http.get<Lesson[]>('/api/lessons', {
+      params: {
+        pageSize: '1000',
+        courseId: courseId.toString()
+      }
+    })
+      .pipe(
+        map(res => res['payload']),
+        shareReplay()
+      );
   }
 
   loadAllCourses(): Observable<Course[]> {
@@ -22,10 +43,22 @@ export class CoursesService {
   }
 
   saveCourse(courseId: string, changes: Partial<Course>): Observable<any> {
-  return this.http.put(`/api/courses/${courseId}`, changes)
-    .pipe(
-      shareReplay()
-    );
+    return this.http.put(`/api/courses/${courseId}`, changes)
+      .pipe(
+        shareReplay()
+      );
   }
 
+  searchLessons(search: string): Observable<Lesson[]> {
+    return this.http.get<Lesson[]>('/api/lessons', {
+      params: {
+        filter: search,
+        pageSize: '100'
+      }
+    })
+      .pipe(
+        map(res => res['payload']),
+        shareReplay()
+      );
+  }
 }
