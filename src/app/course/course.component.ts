22e65bbb8f51148af3d81fcf3f117b82955f6db4
svetlana <svet-ik@ukr.net>
2021-02-06 19:07:03 +0200

    Add change detection.onPush

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,4 +1,4 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
+import {AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Course} from '../model/course';
 import {
@@ -13,35 +13,54 @@ import {
   withLatestFrom,
   concatAll, shareReplay, catchError
 } from 'rxjs/operators';
-import {merge, fromEvent, Observable, concat, throwError} from 'rxjs';
+import {merge, fromEvent, Observable, concat, throwError, combineLatest} from 'rxjs';
 import {Lesson} from '../model/lesson';
 import {CoursesService} from '../services/courses.service';
 
 
+interface CourseData {
+  course: Course;
+  lessons: Lesson[];
+}
+
 @Component({
   selector: 'course',
   templateUrl: './course.component.html',
-  styleUrls: ['./course.component.css']
+  styleUrls: ['./course.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CourseComponent implements OnInit {
 
-  course$: Observable<Course>;
-
-  lessons$: Observable<Lesson[]>;
+ data$: Observable<CourseData>;
 
   constructor(private route: ActivatedRoute,
               private coursesService: CoursesService) {
-
-
   }
 
   ngOnInit() {
     // tslint:disable-next-line:radix
     const courseId = parseInt(this.route.snapshot.paramMap.get('courseId'));
 
-    this.course$ = this.coursesService.loadCourseById(courseId);
+    const course$ = this.coursesService.loadCourseById(courseId)
+      .pipe(
+        startWith(null)
+      );
 
-    this.lessons$ = this.coursesService.loadAllCourseLessons(courseId);
+    const lessons$ = this.coursesService.loadAllCourseLessons(courseId)
+      .pipe(
+        startWith(null)
+      );
+
+    this.data$ = combineLatest([course$, lessons$])
+      .pipe(
+        map(([course, lessons]) => {
+          return {
+            course,
+            lessons
+          };
+        }),
+        tap(console.log)
+      );
   }
 }
 
